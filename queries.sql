2ef21d855cacfc7160b1be7efa03a6bcb8430f58
Steve Renner <che1111@hotmail.com>
2019-11-06 13:21:56 -0800

    Added Multi-Table Queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,24 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select c.CategoryName as Category, p.ProductName as Product
+from [Product] as p
+join Category as c on p.CategoryID = c.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select [Order].Id as OrderId, Shipper.CompanyName from [Order]
+join Shipper on [Order].ShipVia = Shipper.Id
+where ShippedDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select p.ProductName as Product, q.Quantity as OrderDetail
+from [OrderDetail] as q
+join Product as p on q.ProductID = p.Id
+where OrderID = "10251"
+order by ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT [Order].Id, [Customer].CompanyName, [Employee].LastName
+FROM [Order]
+JOIN [Customer], [Employee]
+WHERE [Order].CustomerId = [Customer].Id AND [Order].EmployeeId = [Employee].Id;
